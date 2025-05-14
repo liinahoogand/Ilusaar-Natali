ad4b37dbeba55a7f1ed57ff7ef95243feac3dea7
Liina Hoogand <lhoogand@gmail.com>
2025-05-14 22:03:51 +0300

    hero pilt

--- frontend/src/components/HeroImage.vue
+++ frontend/src/components/HeroImage.vue
@@ -18,7 +18,7 @@
   <style scoped>
 .hero-image {
   height: 80vh;
-  background-image: url('../assets/images/kodulehe-pilt-tekstiga.png');
+  background-image: url('../assets/images/kodulehe-pilt-tekstiga.jpg');
   background-position: center top; /* Nihutab pilti üles */
   background-repeat: no-repeat;
   background-size: cover;
